1f639598e1ecf322bfcf3fa44327717f778aed37
Jv <jvhaileapps@gmail.com>
2018-09-20 15:06:22 +0300

    adjusted some things

---
+++ app/src/main/java/com/innov8/memeit/CustomViews/MemeDraweeView.kt
@@ -0,0 +1,50 @@
+package com.innov8.memeit.CustomViews
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import com.amulyakhare.textdrawable.TextDrawable
+import com.facebook.drawee.generic.RoundingParams
+import com.facebook.drawee.view.SimpleDraweeView
+import com.innov8.memegenerator.utils.fromDPToPX
+
+class ProfileDraweeView:SimpleDraweeView{
+    constructor(context: Context,text:String="",color:Int=Color.RED):super(context){
+        this.text=text
+        this.color=color
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs){
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int):super(context, attrs, defStyle){
+        init()
+    }
+
+
+    lateinit var textDrawable:TextDrawable
+    var text:String=""
+        set(value) {
+            field = value
+            init()
+        }
+    var color:Int= Color.RED
+        set(value) {
+            field = value
+            init()
+        }
+
+    fun init (){
+        textDrawable=TextDrawable
+                .builder()
+                .beginConfig()
+                .bold()
+                .endConfig()
+                .buildRound(text,color)
+
+        val rp=RoundingParams.asCircle()
+        rp.setBorder(Color.WHITE,2f.fromDPToPX(context))
+        hierarchy.roundingParams=rp
+        hierarchy.setPlaceholderImage(textDrawable)
+    }
+}
---
+++ app/src/main/java/com/innov8/memeit/MemeItApp.kt
@@ -0,0 +1,2 @@
+package com.innov8.memeit
+
