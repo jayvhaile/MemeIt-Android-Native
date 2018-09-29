81a5e30a39a1c072b01a07ff06326fb501aa81bf
Jv <jvhaileapps@gmail.com>
2018-09-29 13:26:21 +0300

    integrated facebook native ads

--- app/src/main/java/com/innov8/memeit/MemeItApp.kt
+++ app/src/main/java/com/innov8/memeit/MemeItApp.kt
@@ -1,21 +1,41 @@
 package com.innov8.memeit
 
 import android.app.Application
+import android.os.StrictMode
 import android.preference.PreferenceManager
 import com.cloudinary.android.MediaManager
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.imagepipeline.core.ImagePipelineConfig
 import com.memeit.backend.MemeItClient
 
+
+
 class MemeItApp : Application() {
     companion object {
         lateinit var instanxe:Application
         private const val LOCAL_SERVER_URL = "http://127.0.0.1:5000/api/"
         private const val SERVER_URL = "https://safe-beyond-33046.herokuapp.com/api/"
-
+        private const val DEVELOPER_MODE=true
     }
 
     override fun onCreate() {
+        if (DEVELOPER_MODE) {
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .detectCustomSlowCalls()
+                    .penaltyFlashScreen()
+                    .penaltyLog()
+                    .build())
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build())
+        }
+
         super.onCreate()
         instanxe=this
         MemeItClient.init(applicationContext, SERVER_URL)
